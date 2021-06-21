515058fa98e9f27cf74bf9b83e239d07e6ae60b6
maurya2711 <sharadmaurya4600@gmail.com>
2021-06-21 13:56:31 +0530

    Updates

---
+++ precache-manifest.b53079988c9040f7ef5bfc4840f610e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ecc3cfb2e1c25d8a41c136794d7e21",
+    "url": "/maurya2711/Calculator.git/index.html"
+  },
+  {
+    "revision": "176ad71badc6547ee6a1",
+    "url": "/maurya2711/Calculator.git/static/css/main.86f680a8.chunk.css"
+  },
+  {
+    "revision": "b011ce21a6da794d9886",
+    "url": "/maurya2711/Calculator.git/static/js/2.add7bac3.chunk.js"
+  },
+  {
+    "revision": "176ad71badc6547ee6a1",
+    "url": "/maurya2711/Calculator.git/static/js/main.ab75ea1d.chunk.js"
+  },
+  {
+    "revision": "74cb0e92886073d3adf9",
+    "url": "/maurya2711/Calculator.git/static/js/runtime~main.b78be4cf.js"
+  }
+]);
